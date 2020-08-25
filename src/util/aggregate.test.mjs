a0b752ae9b6ad6cbbdbffe991b70c41ed33bae62
erik <erik@satellitestud.io>
2020-08-25 18:31:11 +0200

    Compare mode

--- src/util/aggregate.test.mjs
+++ src/util/aggregate.test.mjs
@@ -2,10 +2,11 @@ import aggregate from "./aggregate.mjs";
 import tap from 'tap'
 
 const BASE_CONFIG = {
-  breaks: [0, 1, 5, 10, 15, 30],
+  breaks: [[0, 1, 5, 10, 15, 30]],
   delta: 30,
-  geomType: "gridded",
-  interval: "day",
+  geomType: 'gridded',
+  interval: 'day',
+  combinationMode: 'add',
   numDatasets: 1,
   quantizeOffset: 15340,
   singleFrame: false,
@@ -23,18 +24,17 @@ const getAt = (intArray, configOverrides, featureIndex, timeIndex, expect) => {
   )
   const at = agg.features[featureIndex].properties[timeIndex]
   return at
-  tap.equal(
-    at,
-    expect
-  )
 }
 
 tap.equal(getAt([1,1,0,15340,15340,42], {}, 0, 0), 1)
 tap.equal(getAt([1,1,0,15341,15341,42], {}, 0, 0), 1)
 tap.equal(getAt([1,1,0,15341,15341,142], {}, 0, 0), 2)
 tap.equal(getAt([1,1,0,15341,15341,999999], {}, 0, 0), 6)
-tap.equal(getAt([1,1,0,15341,15341,0], {}, 0, 0), 0)
+tap.equal(getAt([1,1,0,15341,15341,0], {}, 0, 0), undefined)
 tap.equal(getAt([1,1,0,15341,15341,42], { breaks: undefined }, 0, 0), .42)
 
-tap.equal(getAt([1,1,0,15341,15341,42,456789], { numDatasets: 2, breaks: undefined }, 0, 0), .42)
-tap.equal(getAt([100,100,0,15341,15341,42,1234,1,15341,15341,43,456789], { numDatasets: 2, breaks: undefined }, 1, 0), .43)
+tap.equal(getAt([1,1,0,15341,15341,42,43], { numDatasets: 2, breaks: undefined }, 0, 0), .85)
+tap.equal(getAt([100,100, 0,15341,15341,42,43, 1,15341,15341,52,53], { numDatasets: 2, breaks: undefined }, 1, 0), 1.05)
+tap.equal(getAt([100,100, 0,15341,15341,42,43], { numDatasets: 2, combinationMode: 'compare', breaks: undefined }, 0, 0), '1,0.43')
+tap.equal(getAt([100,100, 0,15341,15341,52,53], { numDatasets: 2, combinationMode: 'add' }, 0, 0), 2)
+tap.equal(getAt([100,100, 0,15341,15341,52,253], { numDatasets: 2, combinationMode: 'compare', breaks: [[0, 1, 5, 10, 15, 30], [0, 1, 2, 10, 15, 30]] }, 0, 0), 10 + 3)
